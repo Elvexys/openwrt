693a03c78a429e947c278f544ab844d3eb19a6ea
Titouan <titouan.mesot@infoteam.ch>
2018-10-17 15:24:23 +0200

    Choose mtd config and tell sysupgrade script where is the new kernel

--- target/linux/at91/image/lorix_one512.mk
+++ target/linux/at91/image/lorix_one512.mk
@@ -9,13 +9,13 @@ endef
 define Device/at91-sama5d4_lorix_one512
   DEVICE_DTS := at91-sama5d4_lorix_one_512
   $(Device/evaluation-dtb)
-  $(Device/sysupgrade-dtb)
+  $(Device/sysupgrade-dtb-512)
   DEVICE_TITLE := Microchip(Atmel AT91) LORIX One 512MB
   KERNEL_SIZE := 6144k
   BLOCKSIZE := 256k
   PAGESIZE := 4096
   SUBPAGESIZE := 4096
-  MKUBIFS_OPTS := -m $$(PAGESIZE) -e 0x3e000 -c 2082
+  MKUBIFS_OPTS := -m $$(PAGESIZE) -e 0x3e000 -c 4096
   UBINIZE_OPTS := -m $$(PAGESIZE) -p 0x40000 -s 4096
 endef
 TARGET_DEVICES += at91-sama5d4_lorix_one512
--- target/linux/at91/lorix_one512/target.mk
+++ target/linux/at91/lorix_one512/target.mk
@@ -3,6 +3,7 @@ CPU_TYPE:=cortex-a5
 CPU_SUBTYPE:=neon-vfpv4
 FEATURES+=fpu
 DEFAULT_PACKAGES += kmod-usb2 kmod-spi-dev kmod-atmel-usba-udc kmod-usb-serial kmod-usb-gadget-serial
+SUBTARGET:=lorix_one512
 
 define Target/Description
 	Build generic firmware for LORIX ONE 512 MB HW > 1.0d2
